58869c84b9e5ad414576520da94f197a062a0d4d
Vasyl Magometa <vmagometa@reenbit.com>
2020-04-09 12:06:33 +0300

    versioning nuget

---
+++ ExtractPackageVersion.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+    [string] [Parameter(Mandatory = $true)] $projectName
+)
+
+$xml = [Xml] (Get-Content .\$projectName)
+$version = $xml.Project.PropertyGroup.Version
+
+Write-Host $version
+
+if($version) { 
+    $majorVersion, $minorVersion, $patchVersion = $version.ToString().split('.')
+
+	Write-Host "##vso[task.setvariable variable=MajorVersion;]$majorVersion"
+	Write-Host "##vso[task.setvariable variable=MinorVersion;]$minorVersion"
+	Write-Host "##vso[task.setvariable variable=PatchVersion;]$patchVersion"
+}
+
