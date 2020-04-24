431a3c8c30481af8ce1bd9b26a6bec5eb8396b2c
Vasyl Magometa <vmagometa@reenbit.com>
2020-04-24 12:57:06 +0300

    change repo structure

---
+++ src/JsonAutoService/Structures/PostResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace JsonAutoService.Structures
+{
+    public struct PostResult : IRestResult
+    {
+        public bool IsValid
+        {
+            get =>  Id.IsNull ? false : (bool)Id.ToSqlBoolean();
+            set => throw new NotImplementedException();
+        }
+
+        public SqlInt64 Id { get; }
+        public SqlString Body { get; }
+
+        public PostResult(SqlInt64 id, SqlString body)
+        {
+            Id = id;
+            Body = body;
+        }
+    }
+}
